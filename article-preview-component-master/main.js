0b643e654910739f6820a87eef581c1dfe29d516
ChikhladzeLuka <lukachikhladze587@gmail.com>
2025-04-23 23:51:07 +0400

    finish desktop design and finish javascript

--- article-preview-component-master/main.js
+++ article-preview-component-master/main.js
@@ -1,6 +1,16 @@
 const shareBtn = document.querySelector(".share-btn");
 const socialIcons = document.querySelector(".social-icons-window");
+const shareImg = document.querySelector(".share-img");
 
 shareBtn.addEventListener("click", () => {
-  socialIcons.className = ".social-icons-window-visible";
+  if (socialIcons.classList.contains("social-icons-window")) {
+    socialIcons.className = "social-icons-window-visible";
+    shareBtn.style.backgroundColor = "hsl(214, 17%, 51%)";
+    shareImg.style.filter = "brightness(0) invert(1)";
+  } else {
+    socialIcons.className = "social-icons-window";
+    shareBtn.style.backgroundColor = "hsl(210, 46%, 95%)";
+    shareImg.style.filter =
+      "brightness(0.5) sepia(1) saturate(0.5) hue-rotate(180deg)";
+  }
 });
